b2af64bc6b3d7ab63a8d98c83da1a6f6161bd0ba
mdrawde <ema157@uclive.ac.nz>
2021-08-27 18:47:20 +1200

    remvd large code snippet in favor of ref to lsting

---
+++ main/button_COMMENT.c
@@ -0,0 +1,38 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "button.h"
+
+Button_t init_button(uint8_t pin_number, void (*callbackFunction)()) {
+    Button_t button = {
+        .pin_number = pin_number, 
+        .debounce_delay = DEBOUNCE_DELAY, 
+        .last_trigger_time = 0, 
+        .state = true, 
+        .last_state = true, 
+        .callbackFunction = callbackFunction
+    };
+
+    return button;
+}
+
+void button_clicked(Button_t* button) {
+    button->callbackFunction();
+}
+
+void debounce_state(Button_t* button, bool read_button_state, long unsigned current_time) {    |\label{line:button_start}|
+    if (read_button_state != button->last_state) {
+        button->last_trigger_time = current_time;
+    }
+
+    if ((current_time - button->last_trigger_time) > button->debounce_delay) {
+        if (read_button_state != button->state) {
+            button->state = read_button_state;
+            if (button->state) {
+                button_clicked(button);
+            }
+        }
+    }
+
+    button->last_state = read_button_state;
+} |\label{line:button_end}|
